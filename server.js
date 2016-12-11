5fd808288d6609299d6c5c4bd0a898934498ff2e
Emily Main <emilymuirmain@gmail.com>
2016-12-11 12:55:03 -0800

    set up files

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+
+var routes = require('./config/routes');
+var mongoose = require('./config/database');
 
 var app = express();
 
