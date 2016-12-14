f3f3da850bb2e37d897ba22efe9af444d1fa5f91
Emily Main <emilymuirmain@gmail.com>
2016-12-14 15:34:22 -0800

    fixed seeds file and map markers

--- config/seeds.js
+++ config/seeds.js
@@ -1,34 +1,45 @@
-
 require('dotenv').config();
 
 var request = require('request');
 var mongoose = require('./database');
 var Concert = require('../models/Concert');
+var count = 0;
+var allConcerts =[];
 
-request('http://api.jambase.com/events?zipCode=90401&api_key='+ process.env.JAMBASE_KEY+'&o=json', function(error, response, body){
-  var concerts = JSON.parse(body).Events.map(function(ev) {
-    var newConcert = new Concert()
-    newConcert.userCreated = false;
-    newConcert.band = ev.Artists[0].Name;
-    newConcert.venue = ev.Venue.Name;
-    newConcert.address = ev.Venue.Address +", "+ ev.Venue.City + ", "+ ev.Venue.State + ", " + ev.Venue.ZipCode; 
+var myInterval = setInterval(function(){
+  if (count<20) {
+    request('http://api.jambase.com/events?zipCode=90401&api_key='+ process.env.JAMBASE_KEY+'&o=json&page='+count, function(error, response, body){
+      var concerts = JSON.parse(body).Events.map(function(ev) {
+        var newConcert = new Concert()
+        newConcert.userCreated = false;
+        newConcert.band = ev.Artists[0].Name;
+        newConcert.venue = ev.Venue.Name;
+        newConcert.address = ev.Venue.Address +", "+ ev.Venue.City + ", "+ ev.Venue.State + ", " + ev.Venue.ZipCode;
+        newConcert.date = ev.Date;
 
-    return newConcert;
-  });
 
-  Concert.remove({userCreated: false}, function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      Concert.create(concerts, function(err, savedConcerts) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Database seeeded with " + savedConcerts.length + " concerts")
-          mongoose.connection.close();
-        }
-        process.exit()
-      })
-    }
-  })
-})
+
+        return newConcert;
+      });
+      allConcerts=allConcerts.concat(concerts)
+    });
+    count++
+  } else {
+    clearInterval(myInterval)
+    Concert.remove({userCreated: false}, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        Concert.create(allConcerts, function(err, savedConcerts) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Database seeeded with " + savedConcerts.length + " concerts")
+            mongoose.connection.close();
+          }
+          process.exit()
+        })
+      }
+    })
+  }
+}, 1000)
